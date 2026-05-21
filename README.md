f3f536411ce13b056feae4d481207631efdd9761
MicaLC <micaelalc48@gmail.com>
2026-05-20 22:42:58 -0300

    Inicio del proyecto

---
+++ README.md
@@ -0,0 +1,14 @@
+# Chequear todo
+# ☑ 
+
+
+Nombre del Proyecto: Pre-Entrega
+
+Descripción:
+Este proyecto es la pre-entrega de una página web básica desarrollada como parte de un curso de Front-End.
+
+
+La página está estructurada con HTML semántico y utiliza las etiquetas `<header>`,
+`<main>`, y `<footer>` para organizar el contenido. El objetivo es aprender a crear la
+estructura básica de una página web y prepararla para futuras mejoras con CSS y
+JavaScript.
---
+++ comentarios.py
@@ -0,0 +1,13 @@
+#<option>Protector</option><option>Agresivo</option><option>Luchador</option><option>Pícaro</option><option>Erudito</option><option>Líder</option><option>Santo</option><option>Elegido</option>
+#</select>
+#<select name=pals[] multiple=multiple  style='height:300px;'>
+#<option>Débil</option><option>Pesado</option><option>Blando</option><option>Sin carácter</option><option>Lento</option><option>Asocial</option><option>Mundano</option><option>Indefenso</option><option>Estrecho</option>
+#</select>
+#<select name=pals[] multiple=multiple  style='height:300px;'>
+#<option>Fuerte</option><option>Rápido</option><option>Resistente</option><option>Tozudo</option><option>Inteligente</option><option>Social</option><option>Potente</option><option>Defensivo</option><option>Extenso</option>
+
+
+# Comentarios en:
+#       HTML: <!-- -->
+#       CSS:  /*  */
+#       md:   #               ?
